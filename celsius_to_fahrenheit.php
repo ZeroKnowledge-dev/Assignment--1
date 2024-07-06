ffa8eec7de3f23fedc25a57f74dd72bb58070397
Md. Akash Hossen <zeroknowledge.dev@gmail.com>
2024-07-06 14:43:27 +0600

    Add files via upload

---
+++ celsius_to_fahrenheit.php
@@ -0,0 +1,9 @@
+<?php
+// Initialize variables
+$celsius = 32;
+
+// Convert Celsius to Fahrenheit
+$fahrenheit = ($celsius * 9 / 5) + 32;
+
+// Print the result with 2 decimal points
+printf("The temperature in Fahrenheit is: %.2f", $fahrenheit);
